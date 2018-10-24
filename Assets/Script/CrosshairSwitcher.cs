2091d81467ba2b66885a973308e01856597feb0e
GG <b00289996@studentmail.uws.ac.uk>
2018-10-24 20:06:40 +0100

    Added Additional files

---
+++ Assets/Script/CrosshairSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CrosshairSwitcher : MonoBehaviour {
+    // list of available crosshair images
+    public List<Sprite> crosshairs;
+    // the onscreen image to replace
+    public Image crosshairImage;
+
+    // Sets a new crosshair image using the specified index.
+    public void SetImage(int index) {
+        if(index < crosshairs.Count)
+            crosshairImage.sprite = crosshairs[index];
+    }
+}
---
+++ Assets/Script/InteractionBehaviours/DestroyInteraction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyInteraction : InteractionBehaviour {
+
+    public override void Activate() {
+        Destroy(gameObject);
+    }
+}
